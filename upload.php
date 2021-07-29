eef3816acd4a27a901214b363aa6ce65e5e8682c
Drew Gifford <drew@drewgifford.com>
2021-07-29 16:39:07 -0400

    post on upload

--- upload.php
+++ upload.php
@@ -1,16 +1,16 @@
 <?php
 
+    
+
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-    
 
     $servername = "127.0.0.1";
     $username = "letsysml_app";
     $password ="W.6xx@D95kd6Pws";
     $database = "letsysml_lde";
 
-    $root = dirname(__FILE__); 
 
     $conn = new mysqli($servername, $username, $password, $database);
 
@@ -22,15 +22,18 @@
 
     $done = false;
 
+    $file_put_contents("/maps/test.txt", $_GET);
+
     while ($done == false) {
         $id = substr(str_shuffle($str_allowed), 0, 7);
 
-        if (file_exists($root . '/maps/' . $id . '.txt')) {
-            $done = true;
+        if (!file_exists($root . '/maps/' . $id . '.txt')) {
+            break;
         }
     }
 
     $file = fopen($root . '/maps/' . $id . '.txt', "w");
+
 
     fwrite($file, $_GET);
     fclose($file);
