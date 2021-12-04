c82a14753b348b1aa4a96edc99103219e7ab2702
Drew Gifford <drew@drewgifford.com>
2021-12-04 02:16:51 -0500

    updated maps

---
+++ erkling/index.php
@@ -0,0 +1,43 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet">
+<style>
+    body {
+        background: black;
+        color: white;
+        font-family: 'Roboto Mono';
+        font-size: 14px;
+    }
+    img {
+        width: 300px;
+    }
+    div {
+        width: 400px;
+        display: block;
+        margin: auto;
+    }
+</style>
+<div>
+<img src="./Untitled-2.png"/>
+<p>I have almost forgot the taste of fears;<br/>
+The time has been, my senses would have cool'd<br/>
+To hear a night-shriek; and my fell of hair<br/>
+Would at a dismal treatise rouse and stir<br/>
+As life were in't: I have supp'd full with horrors;<br/>
+Direness, familiar to my slaughterous thoughts<br/>
+Cannot once start me.</p>
+<p>
+She should have died hereafter;<br/>
+There would have been a time for such a word.<br/>
+To-morrow, and to-morrow, and to-morrow,<br/>
+Creeps in this petty pace from day to day<br/>
+To the last syllable of recorded time,<br/>
+And all our yesterdays have lighted fools<br/>
+The way to dusty death. Out, out, brief candle!<br/>
+Life's but a walking shadow, a poor player<br/>
+That struts and frets his hour upon the stage<br/>
+And then is heard no more: it is a tale<br/>
+Told by an idiot, full of sound and fury,<br/>
+Signifying nothing.<br/>
+</p>
+</div>
